bb86c64c90f2ee49b18d95de7a60414747d7d1d6
aditya <aditya@gmail.com>
2022-09-15 10:06:57 +0700

    add file upload

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { db } from "../firebase";
 import { collection, addDoc, Timestamp } from "firebase/firestore";
 import { signOut, getAuth } from "firebase/auth";
@@ -7,21 +7,47 @@ import { AuthContext } from "../AuthContext";
 import { useNavigate } from "react-router-dom";
 import Layout from "../components/Layout";
 import moment from "moment/moment";
+import { FileUploader } from "react-drag-drop-files";
+
+const fileTypes = ["JPG", "PNG", "JPEG", "GIF"];
 
 export default function AdminPage() {
+  const dragDrop = (
+    <div className="w-full border-2 border-gray-800 border-dashed rounded cursor-pointer">
+      <div className="py-8 px-10">
+        <p className="text-md text-black text-center  underline underline-offset-2">
+          Drag or drop your image here
+        </p>
+        <p className="text-sm text-black text-center text-opacity-70">
+          (Max size 2 MB)
+        </p>
+      </div>
+    </div>
+  );
+
   const navigate = useNavigate();
   const auth = getAuth();
   const isLogin = useContext(AuthContext);
   const [title, setTitle] = useState("");
+  const [image, setImage] = useState(null);
+  const [fileUrl, setFileUrl] = useState(null);
 
   const { user } = isLogin;
   const joinDate = moment(user?.metadata?.creationTime).fromNow();
 
-  console.log(user);
   const handleSignOut = async () => {
     await signOut(auth).then(() => {
       navigate("/");
     });
+  };
+
+  console.log(fileUrl);
+
+  const handleChange = (event) => {
+    console.log(event);
+    setImage(event);
+    const preview = URL.createObjectURL(event);
+    setFileUrl(preview);
   };
 
   const handleSubmit = async (e) => {
@@ -32,16 +58,26 @@ export default function AdminPage() {
         created: Timestamp.now(),
       });
     } catch (error) {
-      console.log(error);
+      return Promise.reject("Invalid action");
     }
   };
+
+  const handleError = (file) => {
+    console.log(file);
+  };
+
   return (
     <Layout>
-      <div className="py-12">
-        {user && (
-          <>
+      {user && (
+        <>
+          <div className="py-12">
             <div className="flex justify-center w-full">
-              <img src={user?.photoURL} alt="" className="rounded-full" />
+              <img
+                src={user?.photoURL}
+                alt=""
+                className="rounded-full"
+                referrerPolicy="no-referrer"
+              />
             </div>
             <div className="text-center py-2">
               <h1 className="text-lg">{user?.displayName}</h1>
@@ -53,9 +89,24 @@ export default function AdminPage() {
                 Logout
               </button>
             </div>
-          </>
-        )}
-      </div>
+            <div className="w-full flex flex-col items-center space-y-3">
+              <h1 className="text-lg font-semibold">Share your best image</h1>
+              {image && <img src={fileUrl} alt="preview" />}
+              <div>
+                <FileUploader
+                  handleChange={handleChange}
+                  name="image"
+                  types={fileTypes}
+                  multiple={false}
+                  children={dragDrop}
+                  maxSize={1}
+                  onSizeError={handleError}
+                />
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </Layout>
   );
 }
